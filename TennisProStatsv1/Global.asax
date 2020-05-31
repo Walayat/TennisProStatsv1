929f6c5194bb9cb9f29256a451e49a7f8173a570
Asad Abbas <abbasasad309@gmail.com>
2020-05-31 18:08:48 +0500

    Add project files.

---
+++ TennisProStatsv1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TennisProStatsv1.MvcApplication" Language="C#" %>
---
+++ TennisProStatsv1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TennisProStatsv1.Startup))]
+namespace TennisProStatsv1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
